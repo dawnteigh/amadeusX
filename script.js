db3c72028b47b5ef670a814c9def5ac26cb2d447
Donte Ladatto <donte279@gmail.com>
2022-02-11 11:04:11 -0600

    centered, working through nav hover

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+const disappear = $("#disappear");
+$("#text").hover(function(){
+    disappear.hide();
+}, function() {
+    disappear.show();
+});
